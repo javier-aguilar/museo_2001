1a4a250efe7a58af61e58c4591f4e99c5618c361
Javier Aguilar <jvr.agx@gmail.com>
2020-03-04 09:24:12 -0700

    Pass test_it_can_add_photograph

--- lib/curator.rb
+++ lib/curator.rb
@@ -6,4 +6,8 @@ class Curator
     @photographs = []
   end
 
+  def add_photograph(photograph)
+    @photographs << photograph
+  end
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -11,9 +11,29 @@ class CuratorTest < Minitest::Test
     assert_instance_of Curator, curator
   end
 
-
   def test_it_has_attributes
     curator = Curator.new
     assert_equal [], curator.photographs
   end
+
+  def test_it_can_add_photograph
+    curator = Curator.new
+    photo1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    photo2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    })
+    curator.add_photograph(photo1)
+    curator.add_photograph(photo2)
+
+    assert_equal [photo1, photo2], curator.photographs
+  end
+
 end
