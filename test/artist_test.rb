ae3041f4ce0aa5ac2bdeaadd673275bb84918344
Javier Aguilar <jvr.agx@gmail.com>
2020-03-04 09:14:16 -0700

    Pass test_it_has_attributes

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -17,9 +17,21 @@ class ArtistTest < Minitest::Test
     assert_instance_of Artist, artist
   end
 
-
   def test_it_has_attributes
-    skip
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_equal "2", artist.id
+    assert_equal "Ansel Adams", artist.name
+    assert_equal "1902", artist.born
+    assert_equal "1984", artist.died
+    assert_equal "United States", artist.country
   end
 
   def test_it_can_calculate_age_of_death
