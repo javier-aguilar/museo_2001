14070ed20a034b10cad8aa71895b535f76d062c0
Javier Aguilar <jvr.agx@gmail.com>
2020-03-04 09:20:08 -0700

    Pass test_it_exists

---
+++ test/curator_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require './lib/photograph'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+
+  def test_it_exists
+    curator = Curator.new
+    assert_instance_of Curator, curator
+  end
+
+
+  def test_it_has_attributes
+    skip
+  end
+end
