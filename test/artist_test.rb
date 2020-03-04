57fd3965f1b5d28571d820e9e4f9a5268c5fd995
Javier Aguilar <jvr.agx@gmail.com>
2020-03-04 09:17:17 -0700

    Add setup to ArtistTest

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -5,7 +5,7 @@ require './lib/artist'
 
 class ArtistTest < Minitest::Test
 
-  def test_it_exists
+  def setup
     attributes = {
       id: "2",
       name: "Ansel Adams",
@@ -13,37 +13,22 @@ class ArtistTest < Minitest::Test
       died: "1984",
       country: "United States"
     }
-    artist = Artist.new(attributes)
-    assert_instance_of Artist, artist
+    @artist = Artist.new(attributes)
+  end
+
+  def test_it_exists
+    assert_instance_of Artist, @artist
   end
 
   def test_it_has_attributes
-    attributes = {
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    }
-    artist = Artist.new(attributes)
-
-    assert_equal "2", artist.id
-    assert_equal "Ansel Adams", artist.name
-    assert_equal "1902", artist.born
-    assert_equal "1984", artist.died
-    assert_equal "United States", artist.country
+    assert_equal "2", @artist.id
+    assert_equal "Ansel Adams", @artist.name
+    assert_equal "1902", @artist.born
+    assert_equal "1984", @artist.died
+    assert_equal "United States", @artist.country
   end
 
   def test_it_can_calculate_age_of_death
-    attributes = {
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    }
-    artist = Artist.new(attributes)
-
-    assert_equal 82, artist.age_at_death
+    assert_equal 82, @artist.age_at_death
   end
 end
